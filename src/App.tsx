3b55c2a8f9040dcff11320680692c35f5f434d6c
Egorych <mikhalevichegor@gmail.com>
2024-03-12 22:20:11 +0300

    sprint 01, week 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import './App.css';
+import {Todolist} from "./components/Todolist";
+
 function App() {
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = []
 
     return (
         <div className="App">
-
+            <Todolist title="What to learn" tasks={tasks1} />
+            <Todolist title="Songs" tasks={tasks2} />
         </div>
     );
 }
 
 export default App;
-
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import {dividerClasses} from "@mui/material";
 
-export function Todolist() {
-    return (
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export function Todolist({title, tasks}: PropsType) {
+
+    return <div>
+
+        <h3>{title}</h3>
         <div>
-
+            <input/>
+            <button>+</button>
         </div>
-        )
+        <ul>
+            {
+                tasks.length === 0 ? (
+                    <p>No tasks</p>
+                ) : (
+                    <ul>
+                        {tasks.map((task: TaskType) => {
+                            return (
+                                <li>
+                                    <input type="checkbox" checked={task.isDone}/>
+                                    <span>{task.title}</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                )
+            }
+            {/*<li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span></li>
+            <li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span></li>*/}
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
 }
