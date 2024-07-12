04923c993918e52c494eefeb41f314ce5c279d11
Egorych <mikhalevichegor@gmail.com>
2024-07-12 23:49:00 +0300

    function removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./components/Todolist";
+import {TasksType, Todolist} from "./components/Todolist";
 
 function App() {
     const titleTodo = 'What to learn'
-    const tasks = [
-        { id: 1, title: 'HTML&CSS', isDone: true },
-        { id: 2, title: 'JS', isDone: true },
-        { id: 3, title: 'ReactJS', isDone: false },
-        { id: 4, title: 'Redux', isDone: false }
-    ]
+   const [tasks, setTasks] = useState<TasksType[]>(
+        [
+            { id: 1, title: 'HTML&CSS', isDone: true },
+            { id: 2, title: 'JS', isDone: true },
+            { id: 3, title: 'ReactJS', isDone: false },
+            { id: 4, title: 'Redux', isDone: false }
+        ]
+    )
+    const removeTask = (id: number) => {
+        let task = tasks.filter(t => t.id !== id)
+        setTasks([...task])
+    }
     return (
         <div className="App">
             <Todolist
                 title={titleTodo}
+                removeTask={removeTask}
                 tasks={tasks}
             />
         </div>
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -4,14 +4,16 @@ import {Button} from "./Button";
 type TodolistPropsType = {
     title: string
     tasks: Array<TasksType>
+    removeTask: (id: number)=>void
+
 }
-type TasksType = {
+export type TasksType = {
     id: number
     title: string
     isDone: boolean
 }
 
-export function Todolist({title, tasks}: TodolistPropsType) {
+export function Todolist({title, tasks, removeTask}: TodolistPropsType) {
     const tasksMapping: Array<JSX.Element> | JSX.Element = tasks.length !==0
       ?  tasks.map((task => {
         return <li key={task.id}>
@@ -20,7 +22,7 @@ export function Todolist({title, tasks}: TodolistPropsType) {
                 checked={task.isDone}/>
             <span>{task.title}</span>
             <Button title={'✘'} callback={() => {
-                console.log(task.id)}}/>
+                removeTask(task.id)}}/>
         </li>
     }))
             : <span>Your tasks is empty</span>
